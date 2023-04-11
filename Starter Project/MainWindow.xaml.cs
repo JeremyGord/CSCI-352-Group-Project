ea6deee74d63a4f1b2cba46aaf2c2bd968183767
Jeremy Gordon <jerrgord@ut.utm.edu>
2023-04-11 15:46:08 -0500

    Progress!!!

--- Starter Project/MainWindow.xaml.cs
+++ Starter Project/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Globalization;
 
 namespace Starter_Project
 {
@@ -20,16 +22,48 @@ namespace Starter_Project
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public int MyValue { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = this;
         }
 
-        // Doesn't update textbox. Unknown
-        //private void SelectedDatesChanged(object sender, SelectionChangedEventArgs e)//RoutedEventArgs e)
-        //{
-            //myTextBox.Text = myCalendar.SelectedDates.ToString();
-            //Console.WriteLine(myCalendar.SelectedDates.ToString());
-        //}
+
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button1_Click_1(object sender, RoutedEventArgs e)
+        {
+            String filename = textbox1.Text;
+            if (File.Exists(filename))
+            {
+                BitmapImage theImage = new BitmapImage
+            (new Uri(filename, UriKind.Relative));
+
+                ImageBrush myImageBrush = new ImageBrush(theImage);
+                Calender1.Background = myImageBrush;
+            }
+            else
+            {
+                MessageBox.Show("File path does not exist");
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Calender1.Background = Brushes.White;
+        }
+
+        private void Add_Event_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 secondWindow = new Window1();
+            secondWindow.Show();
+        }
     }
 }
