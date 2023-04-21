db2b9c81b2431aa2d9ec92f2b4c69dceae86d462
kylhbyas <kylhbyas@ut.utm.edu>
2023-04-20 22:22:35 -1000

    added functionality

--- Starter Project/Window1.xaml.cs
+++ Starter Project/Window1.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,14 +22,84 @@ namespace Starter_Project
     /// </summary>
     public partial class Window1 : Window
     {
+        string start;
+        string end;
+        string color;
+        string title;
+
         public Window1()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Cancel_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
+            Close();
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            string[] lines = { title, Picker.Text, start, end , color };
+            
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+            using (StreamWriter outputFile = new StreamWriter(System.IO.Path.Combine(docPath, "EventList.txt"), true))
+            {
+                foreach (string line in lines)
+                {
+                    outputFile.WriteLine(line);
+                }
+            }
+
+            Close();
+        }
+
+        private void ComboBox_Start_TimeChanged(object sender, SelectionChangedEventArgs e)
+        {
+            start = Start_Time.SelectedItem.ToString();
+        }
+
+        private void ComboBox_End_TimeChanged(object sender, SelectionChangedEventArgs e)
+        {
+            end = End_Time.SelectedItem.ToString();
+        }
+
+        private void RedRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            color = "Red";
+        }
+
+        private void OrangeRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            color = "#FFFF7400";
+        }
+
+        private void YellowRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            color = "#FFFFB900";
+        }
+
+        private void GreenRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            color = "#FF00FF0C";
+        }
+
+        private void TurquoiseRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            color = "#FF00FFC5";
+        }
+
+        private void BlueRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            color = "#FF0097FF";
+        }
+
+        private void TitleTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Title_Textbox.Text != "Title" && Title_Textbox.Text != null)
+            {
+                title = Title_Textbox.Text;
+            }
         }
     }
 }
